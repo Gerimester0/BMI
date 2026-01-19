a7643a27aa7851458c1ec53ccac64798bd925ecb
PappG <PappG@310-14>
2026-01-19 08:05:33 +0100

    Diák adatszerkezet létrehozása

---
+++ BMI/diak.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI
+{
+    public class Diak
+    {
+        public string Név { get; set; }
+        public int Életkor { get; set; }
+
+        public int Magasság { get; set; }
+
+        public int Testsúly { get; set; }
+
+        public Diak(string név, int életkor, int magasság, int testsúly)
+        {
+            Név = név;
+            Életkor = életkor;
+            Magasság = magasság;
+            Testsúly = testsúly;
+        }
+
+        public override string? ToString()
+        {
+            return $"{Név} {Életkor}éves {Magasság}cm {Testsúly}kg";
+        }
+    }
+}
