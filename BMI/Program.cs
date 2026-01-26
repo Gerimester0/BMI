406ca65e1d3b82c87962b9918f544ad1975d9187
PappG <PappG@310-14>
2026-01-26 08:35:44 +0100

    5.a átlag testsúly

--- BMI/Program.cs
+++ BMI/Program.cs
@@ -1,13 +1,15 @@
-﻿namespace BMI
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace BMI
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             List<Diak> list = new List<Diak>();
-            var sorok = File.ReadAllLines("BMI.txt",System.Text.Encoding.Latin1).Skip(1);
+            var sorok = File.ReadAllLines("BMI.txt", System.Text.Encoding.Latin1).Skip(1);
 
-            foreach (var sor in sorok) 
+            foreach (var sor in sorok)
             {
                 Console.WriteLine(sor);
                 string[] darabok = sor.Split(";");
@@ -15,10 +17,33 @@
                 int eletkor = int.Parse(darabok[1]);
                 int magassag = int.Parse(darabok[2]);
                 int suly = int.Parse(darabok[3]);
-                Diak d = new Diak(nev,eletkor,magassag,suly);
+                Diak d = new Diak(nev, eletkor, magassag, suly);
                 list.Add(d);
 
             }
+            foreach (var d in list)
+            {
+                Console.WriteLine(d);
+            }
+            Console.WriteLine("-------------");
+            Console.WriteLine($"3. a feladat: A diákok száma: {list.Count}");
+
+            Diak legmagasabb = list[0];
+            foreach (var diak in list)
+            {
+                if (diak.Magasság > legmagasabb.Magasság)
+                    legmagasabb = diak;
+            }
+            Console.WriteLine($"3. b feladat: A legmagasabb diák:" + $"{legmagasabb.Név}, magasság: {legmagasabb.Magasság} cm ");
+
+            double atlag = 0;
+            foreach (var diak in list)
+            {
+                atlag += diak.Testsúly;
+            }
+            atlag /= list.Count;
+            Console.WriteLine($"5. a feladat átlagos testsúly:"+ $"{atlag:F1} kg");
+
         }
     }
 }
--- BMI/diak.cs
+++ BMI/diak.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,5 +28,28 @@ namespace BMI
         {
             return $"{Név} {Életkor}éves {Magasság}cm {Testsúly}kg";
         }
+        public string BMI()
+        {
+            double magassagm = Magasság / 100.0;
+            double bmi = Testsúly / (magassagm * magassagm);
+                if (bmi < 18.5)
+                {
+                    return "sovány";
+                }
+                else if (bmi < 25)
+                {
+                    return "Normal";
+                }
+                else if (bmi < 30 )
+                {
+                    return "Túlsúly";
+                }
+                else
+                {
+                return "Elhízott";
+            }
+        }
+        
+        
     }
 }
